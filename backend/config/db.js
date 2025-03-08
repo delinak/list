fb2ac094ce413271f730cf146d62fa78e9b5b39f
delinak <delina.kiflom14@gmail.com>
2025-03-08 12:53:21 -0500

    initial commit

---
+++ backend/config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://0.0.0.0:27017/todolistDB');
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB; 
